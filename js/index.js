4947f9437dfd84b4a253cc784e2be2ea0db230df
tbernard4 <tbernard@udesa.edu.ar>
2024-11-06 14:31:30 -0300

    Fecth continuacion

--- js/index.js
+++ js/index.js
@@ -2,7 +2,27 @@ fetch("https://dummyjson.com/recipes")
   .then(function (response) {
     return response.json();
   })
+
+.then(function (data) {
+  let recetas = data.results;
+  let characters = "";
+  let characterList = document.querySelector(".character-list");
+  characterList.style.display = "flex"
+  characterList.style.flexWrap = "wrap"
+  characterList.style.justifyContent = "space-evenly"   
+   
+  for (let i = 0; i < recetas.length; i++) {
+    characters += `
+              <article>
+                     <img src= ${personajes[i].image} alt=''>
+                      <p>Name: ${personajes[i].name} </p>
+                      <p>Status:${personajes[i].status} </p>
+               </article>
+           `;
+  }
+  console.log(characters);
+  characterList.innerHTML = characters
+})
   .catch(function (error) {
     console.log("error: ", error);
   });
-  
